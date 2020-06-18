1faff974c3e45eba42b8e46d43f3b39022f4c634
Felix Anducho <felix.anducho74@gmail.com>
2020-06-17 19:38:28 -0500

    6.8: anecdotes, step6
    Separate the rendering of the anecdote list into its own component called AnecdoteList.

--- src/App.js
+++ src/App.js
@@ -1,54 +1,14 @@
 import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { createAnecdote, voteAnecdote} from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
-  const dispatch = useDispatch()
-
-  const compare = (a, b) => {
-    const anecdoteA = a.votes
-    const anecdoteB = b.votes
-
-    let comparison = 0
-    if (anecdoteA < anecdoteB){
-      comparison = 1
-    } else if (anecdoteA > anecdoteB){
-      comparison = -1
-    }
-    return comparison
-  }
-
-  const anecdotesSortedByVotes = anecdotes.sort(compare)
-
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
-  }
-
-  const vote = (id) => {
-    dispatch(voteAnecdote(id))
-  }
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotesSortedByVotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
-      <h2>create new anecdote</h2>
-      <AnecdoteForm addAnecdote={addAnecdote}></AnecdoteForm>
+      <AnecdoteList></AnecdoteList>
+      <AnecdoteForm></AnecdoteForm>
     </div>
   )
 }
---
+++ src/components/AnecdoteList.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { useSelector, useDispatch } from 'react-redux'
+import { voteAnecdote } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = () => {
+    const anecdotes = useSelector(state => state)
+    const dispatch = useDispatch()
+
+    const compare = (a, b) => {
+        const anecdoteA = a.votes
+        const anecdoteB = b.votes
+
+        let comparison = 0
+        if (anecdoteA < anecdoteB){
+            comparison = 1
+        } else if (anecdoteA > anecdoteB){
+            comparison = -1
+        }
+    return comparison
+  }
+  const vote = (id) => {
+    dispatch(voteAnecdote(id))
+  }
+
+  const anecdotesSortedByVotes = anecdotes.sort(compare)
+  return (
+    anecdotesSortedByVotes.map(anecdote =>
+    <div key={anecdote.id}>
+      <div>{anecdote.content}</div>
+      <div>
+        has {anecdote.votes}
+        <button onClick={() => vote(anecdote.id)}>vote</button>
+      </div>
+    </div>
+    )
+  );
+};
+
+export default AnecdoteList
